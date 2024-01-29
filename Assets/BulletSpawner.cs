97489bd3fe8c809e245526908ffda0bfd0fc8374
Park JunHyoung <matte1428@naver.com>
2024-01-29 18:00:05 +0900

    feat:BulletSpawner

--- Assets/BulletSpawner.cs
+++ Assets/BulletSpawner.cs
@@ -14,12 +14,22 @@ public class BulletSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<PlayerController>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeAfterSpawn += Time.deltaTime;
+
+        if(timeAfterSpawn >= spawnRate) {
+            timeAfterSpawn = 0f;
+            GameObject bullet = Instantiate(bulletPrefab,transform.position,transform.rotation);
+            bullet.transform.LookAt(target);
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
     }
 }
