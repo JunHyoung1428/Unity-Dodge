6f489d69cc8b308e6970624623bb67637fb239d9
Park JunHyoung <matte1428@naver.com>
2024-01-30 14:32:16 +0900

    update

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
+    public ParticleSystem explosion;
     public float speed = 8.0f;
+    public float jumpForce = 20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +27,26 @@ public class PlayerController : MonoBehaviour
         Vector3 newVelocity = new Vector3 (xSpeed,0f, zSpeed);
 
         playerRigidbody.velocity = newVelocity; //가속도를 줌
+       
+        if (Input.GetKey(KeyCode.Space)==true)
+        {
+            playerRigidbody.velocity =Vector3.up*jumpForce;
+        }
     }
 
     public void Die()
     {
+        
+        if (explosion != null)
+        {
+           explosion.transform.position = playerRigidbody.transform.position;
+           explosion.Play();
+           Debug.Log("play explosion");
+        }
         //자신의 오브젝트 비활성화
         gameObject.SetActive(false);
+
+        GameManager gameManager  = FindObjectOfType<GameManager>();     
+        gameManager.EndGame();
     }
 }
